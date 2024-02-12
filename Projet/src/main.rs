33a2a4c0f7aae73f4e5cb42a8e752ab7afdb7a68
peneloux <nicolas.peneloux@free.fr>
2024-02-12 18:42:51 +0100

    nettoyage, test, modification de l'algo fft

--- Projet/src/main.rs
+++ Projet/src/main.rs
@@ -1,5 +1,26 @@
 mod GUI;
+mod wav_reader;
+use crate::wav_reader::wav_reader::WavReader;
 
 fn main() {
-    GUI::gui::launch();
+
+    /* TEST DU GUI */
+    //GUI::gui::launch();
+
+
+
+
+
+
+
+    /* TEST DU WAV_READER */
+    let file_path = "../resource/3s.wav";
+    match WavReader::new_reader(file_path) {
+        Ok(reader) => {
+            let spectrum = reader.fft();
+            println!("Spectre de fréquences: {:?}", spectrum);
+        }
+        Err(e) => println!("Erreur: {}", e),
+    }
+    
 }
